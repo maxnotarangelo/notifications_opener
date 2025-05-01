647327fadc453e20f19a879ced6f421015f053dc
Max Notarangelo <mnotarangelo@allumia.com>
2025-05-01 11:49:48 -0700

    use `CGI.unescape` instead of `URI.decode`
    The `decode` method on `URI` got deleted in some version of Ruby (possibly as early as Ruby 2).

--- lib/notifications_opener/handler/sms.rb
+++ lib/notifications_opener/handler/sms.rb
@@ -30,7 +30,7 @@ module NotificationsOpener
         {
           from: p[config[:from_key_name]],
           to: p[config[:to_key_name]],
-          message: URI.decode(p[config[:message_key_name]]),
+          message: CGI.unescape(p[config[:message_key_name]]),
           location: config[:location]
         }
       end
